d2dc226541c5e01e43a6d22cf81711474a362743
fbmnds <fbmnds7@gmail.com>
2017-10-15 13:24:38 +0200

    Get Gab.ai auth token

---
+++ Gabai.fs
@@ -0,0 +1,26 @@
+﻿module Gabai
+
+open System
+open System.Net
+open System.IO
+open System.Xml.XPath
+
+let loginUri = "https://gab.ai/auth/login"
+
+let deleteLines linesToRemove (text : string) = 
+    text.Split(Environment.NewLine.ToCharArray(), linesToRemove + 1)
+    |> Seq.tail
+    |> String.concat ""
+
+
+let pattern = """<input type="hidden" name="_token" value="""
+let position = 5
+
+let getToken () = 
+    let req  = WebRequest.Create(loginUri, Method="GET")
+    use resp = req.GetResponse()
+    use strm = new StreamReader(resp.GetResponseStream())
+    strm.ReadToEnd().Split(Environment.NewLine.ToCharArray())
+    |> Array.filter (fun s -> s.Contains(pattern))
+    |> String.concat ""
+    |> fun t -> t.Trim([|' ';'<';'>';'"'|]).Split([|'"'|]).[position]
--- Program.fs
+++ Program.fs
@@ -51,16 +51,32 @@ let handleTwitterFeed name (next: HttpFunc) (ctx: HttpContext) =
         return! text tweets next ctx
     }
 
+<<<<<<< HEAD
+let handleGabThumbnail name feed (next: HttpFunc) (ctx: HttpContext) =
+    task {
+        let std,err = Thumbnail.execute name feed
+        return! text (sprintf "%s\n%s" std err) next ctx
+    }
+=======
+>>>>>>> dev
+
+let handleTwitterPost (next: HttpFunc) (ctx: HttpContext) =
+    task {
+        let! post =  ctx.BindJson<Twitter.Post>()
+        return! text (Twitter.postTweet post) next ctx
+    }
+
+
 let handleGabThumbnail name feed (next: HttpFunc) (ctx: HttpContext) =
     task {
         let std,err = Thumbnail.execute name feed
         return! text (sprintf "%s\n%s" std err) next ctx
     }
 
-let handleTwitterPost (next: HttpFunc) (ctx: HttpContext) =
+
+let handleGabLogin (next: HttpFunc) (ctx: HttpContext) =
     task {
-        let! post =  ctx.BindJson<Twitter.Post>()
-        return! text (Twitter.postTweet post) next ctx
+        return! text (Gabai.getToken ()) next ctx
     }
 
 
@@ -86,6 +102,10 @@ let webApp =
         POST >=> route  "/tweets/post"    >=> handleTwitterPost
         GET  >=> routef "/tweets/feed/%s" (fun name -> (handleTwitterFeed name))
 
+<<<<<<< HEAD
+=======
+        GET  >=> route  "/gab/login"           >=> handleGabLogin
+>>>>>>> dev
         GET  >=> routef "/gab/thumbnail/%s/%s" (fun (name,post) -> (handleGabThumbnail name post))
         
         GET  >=> route  "/github/repos"         >=> handleGithub
